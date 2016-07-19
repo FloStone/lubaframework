facd8dd381d5c832e9d49b6628bf2ff0db57c5c1
Florian Steiner <fs@adiwidjaja.com>
2016-07-19 14:22:38 +0200

    Improved mysql handling

--- aliases/SQL.php
+++ aliases/SQL.php
@@ -4,13 +4,8 @@ use Flo\MySQL\MySQL;
 
 class SQL
 {
-	protected static $sqlInstance;
-
 	public static function __callStatic($func, $args)
-	{	
-		if (!self::$sqlInstance)
-			self::$sqlInstance = MySQL::connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-
-		return call_user_func_array([self::$sqlInstance, $func], $args);
+	{
+		return call_user_func_array([new MySQL(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE), $func], $args);
 	}
 }
