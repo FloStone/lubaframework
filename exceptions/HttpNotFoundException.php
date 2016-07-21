80e185fbb6bb6923f54db86f5fb3d245f750a2a9
Florian Steiner <fs@adiwidjaja.com>
2016-07-21 15:23:27 +0200

    Changed http response code on 404, updated dependencies

--- exceptions/HttpNotFoundException.php
+++ exceptions/HttpNotFoundException.php
@@ -6,6 +6,7 @@ class HttpNotFoundException extends \Exception
 {
 	public function __construct($url)
 	{
+		http_response_code(404);
 		parent::__construct("The URL \"$url\" was not found on this server!");
 	}
 }
