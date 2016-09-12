8fa000868f6a7a9fae09a545d7cf9df289624de0
Florian Steiner <fs@adiwidjaja.com>
2016-09-12 12:35:44 +0200

    Added command execute function

--- luba/Command.php
+++ luba/Command.php
@@ -6,5 +6,14 @@ use Luba\Interfaces\CommandInterface;
 
 abstract class Command implements CommandInterface
 {
+	public static function exec($name)
+	{
+		$command = "Luba\Commands\\$name";
 
+		if (!class_exists($command))
+			throw new \Exception("Command $name not found!");
+
+		$command = new $command;
+		$command->run();
+	}
 }
