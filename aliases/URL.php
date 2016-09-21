0d86c3e667f3a48ca91574187ab5a86901d9a85f
Florian Steiner <fs@adiwidjaja.com>
2016-09-21 16:05:48 +0200

    Fixed static

--- aliases/URL.php
+++ aliases/URL.php
@@ -4,7 +4,7 @@ use Luba\Framework\URL as BaseURL;
 
 class URL
 {
-	public function __callStatic($method, $args)
+	public static function __callStatic($method, $args)
 	{
 		return call_user_func_array([BaseURL::getInstance(), $method], $args);
 	}
