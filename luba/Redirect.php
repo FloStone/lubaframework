91ef8a53da98c47baef6dec9124c227503b449a4
Florian Steiner <fs@adiwidjaja.com>
2016-09-25 15:45:35 +0200

    Fixed redirect types

--- luba/Redirect.php
+++ luba/Redirect.php
@@ -6,7 +6,7 @@ class Redirect
 {
 	public static function to($url, $type = 302)
 	{
-		$this->getRedirectType($type);
+		static::getRedirectType($type);
 
 		return header("Location: $url");
 	}
@@ -14,12 +14,12 @@ class Redirect
 	public static function external($url, $type = 302)
 	{
 		$url = \URL::other($url);
-		$this->getRedirectType($type);
+		static::getRedirectType($type);
 		
 		return header("Location: $url");
 	}
 
-	public function getRedirectHeader($type)
+	public static function getRedirectHeader($type)
 	{
 		if ($type == 301)
 			header("HTTP/1.1 301 Moved Permanently");
