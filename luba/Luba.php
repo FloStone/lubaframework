cc6afe67eb77380ba84e9add6469d04b10a73144
Florian Steiner <fs@adiwidjaja.com>
2016-10-07 16:22:52 +0200

    Application now extends Luba class, New method for loading config file

--- luba/Luba.php
+++ luba/Luba.php
@@ -4,6 +4,20 @@ namespace Luba\Framework;
 
 abstract class Luba
 {
+	public function __construct()
+	{
+		$this->loadconfigs();
+	}
+
+	public function loadconfigs($file)
+	{
+		if (file_exists(base_path('config/config.php')))
+			require base_path('config/config.php');
+
+		if (file_exists(base_path('config/global.php')))
+			require base_path('config/global.php');
+	}
+
 	public static function runCommand($command)
 	{
 		return Command::exec($command);
