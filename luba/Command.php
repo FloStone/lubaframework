6e40bc34563a4dc6664977816ac97c6d28622525
Florian Steiner <fs@adiwidjaja.com>
2016-12-13 15:58:11 +0100

    Added terminal colors

--- luba/Command.php
+++ luba/Command.php
@@ -6,10 +6,28 @@ use Luba\Interfaces\CommandInterface;
 
 abstract class Command implements CommandInterface
 {
+	const COLOR_DEFAULT = "\033[0m";
+	const COLOR_BLUE = "\033[34m";
+	const COLOR_GREEN = "\033[32m";
+	const COLOR_RED = "\033[31m";
+	const COLOR_PURPLE = "\033[35m";
+
+	/**
+	 * Command options
+	 * @var array
+	 */
 	protected static $options = [];
 
+	/**
+	 * Command Arguments
+	 * @var array
+	 */
 	protected static $arguments = [];
 
+	/**
+	 * Set the command arguments
+	 * @param array $args
+	 */
 	public static function setArguments(array $args = [])
 	{
 		unset($args[0]);
@@ -18,6 +36,11 @@ abstract class Command implements CommandInterface
 		static::$arguments = $args;
 	}
 
+	/**
+	 * Run a command
+	 * @param  string $name
+	 * @return void
+	 */
 	public static function runCommand($name)
 	{
 		$command = "Luba\Commands\\$name";
@@ -29,17 +52,32 @@ abstract class Command implements CommandInterface
 		$command->run();
 	}
 
-	public function output($output)
+	/**
+	 * Print something to console
+	 * @param  string $output
+	 * @return void
+	 */
+	public function output($output, $color = self::COLOR_DEFAULT)
 	{
-		print $output;
+		print $color . $output;
 		print "\n";
 	}
 
+	/**
+	 * Execute a command
+	 * @param  string $name
+	 * @return void
+	 */
 	public function exec($name)
 	{
 		static::runCommand($name);
 	}
 
+	/**
+	 * Get a command argument
+	 * @param  string|int $index
+	 * @return string
+	 */
 	public function argument($index)
 	{
 		return isset(static::$arguments[$index]) ? static::$arguments[$index] : null;
