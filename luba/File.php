ebae1a8655d6c4016734c726367f99596c100285
Florian Steiner <fs@adiwidjaja.com>
2016-12-13 12:36:35 +0100

    Fixed file inheritance

--- luba/File.php
+++ luba/File.php
@@ -4,12 +4,42 @@ namespace Luba\Framework;
 
 class File
 {
-	protected $name;
+	/**
+	 * File size
+	 *
+	 * @var int
+	 */
+	protected $size;
 
+	/**
+	 * File type
+	 *
+	 * @var string
+	 */
+	protected $type;
+
+	/**
+	 * File extension
+	 * @var string
+	 */
 	protected $extension;
 
+	/**
+	 * File name
+	 * @var string
+	 */
+	protected $name;
+
+	/**
+	 * Path of the file
+	 * @var string
+	 */
 	protected $path;
 
+	/**
+	 * Constructor
+	 * @param string $path
+	 */
 	public function __construct($path = "")
 	{
 		$this->path = $path;
@@ -17,8 +47,85 @@ class File
 		$this->extension = pathinfo($path, PATHINFO_EXTENSION);
 	}
 
+	/**
+	 * Create a new File instance
+	 * @param  string $path
+	 * @return File
+	 */
 	public static function make($path)
 	{
 		return new self($path);
+	}
+
+	/**
+	 * get the file type
+	 *
+	 * @return string
+	 */
+	public function type()
+	{
+		return $this->type;
+	}
+
+	/**
+	 * Get the file extension
+	 *
+	 * @return string
+	 */
+	public function extension()
+	{
+		return $this->extension;
+	}
+
+		/**
+	 * Get the file size
+	 *
+	 * @return int
+	 */
+	public function size()
+	{
+		return $this->size;
+	}
+
+	/**
+	 * Get the file path
+	 *
+	 * @return string
+	 */
+	public function path()
+	{
+		return $this->path;
+	}
+
+	/**
+	 * Rename the file
+	 *
+	 * @param string $name
+	 * @return void
+	 */
+	public function rename($name)
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	/**
+	 * Get the full name including extension
+	 *
+	 * @return string
+	 */
+	public function fullName()
+	{
+		return "{$this->name}.{$this->extension}";
+	}
+
+	/**
+	 * Name of the file
+	 * @return String name
+	 */
+	public function name()
+	{
+		return $this->name;
 	}
 }
